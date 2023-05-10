fdcfd5a78107b70467a9ef79549bd0933da0f98a
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-05-10 20:43:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    int n = 0,primo = 1,secondo = 1,terzo = 0;
+    cout << "scegli il valore" << endl;
+    cin >> n;
+    bool finito = true;
+    if (n >= 2){
+        cout << primo << endl;
+        cout << secondo <<endl;
+        while (n-2 > 0 && finito){
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            if(terzo <= n){
+                    cout << terzo << endl;
+            }
+            else{
+                finito = false;
+            }
+
+            n--;
+        }
+    }
+
+    else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
